6211617699cc04a82b2aa8d2630640f8ad575b55
yujeonglee623 <yujeonglee623@gmail.com>
2025-12-24 15:14:36 +0900

    to-do 앱 완성

--- todos/main.py
+++ todos/main.py
@@ -49,16 +49,42 @@ def home(request: Request,
                                       {"request": request,
                                        "todos": todos})
 
+# 수정을 위한 조회
+@app.get("/edit/{todo_id}")
+def edit(request: Request, todo_id: int, db: Session = Depends(get_db)):
+    todo = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
+    todos = db.query(models.Todo).order_by(models.Todo.id.desc()).all()
+    return templates.TemplateResponse("edit.html", {"request": request, "todo": todo, "todos": todos})
+
+# 수정한 것 적용
+@app.post("/edit/{todo_id}")
+def update(request: Request, todo_id: int, task: str = Form(...), 
+                 completed: bool = Form(False), db: Session = Depends(get_db)):
+    todo = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
+    todo.task = task
+    todo.completed = completed
+    db.commit()
+    return RedirectResponse(url=app.url_path_for("home"), 
+                            status_code=status.HTTP_303_SEE_OTHER)
+
 @app.post('/add')
 def home(request: Request,
          task: str = Form(...),
          db: Session = Depends(get_db)):
     # todo 객체 생성
     todo = models.Todo(task=task)
-    print(todo)
+    # print(todo)
     # todo를 db 추가
     db.add(todo)
     # db 커밋(db에 반영)
+    db.commit()
+    return RedirectResponse(url=app.url_path_for("home"),
+                            status_code=status.HTTP_303_SEE_OTHER)
+
+@app.get('/delete/{todo_id}')
+def delete(request: Request, todo_id: int, db: Session = Depends(get_db)):
+    todo = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
+    db.delete(todo)
     db.commit()
     return RedirectResponse(url=app.url_path_for("home"),
                             status_code=status.HTTP_303_SEE_OTHER)
